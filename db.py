6738c66bf5565a988f121d77bbe0fa5936a772ab
Mustafo <example@gmail.com>
2024-05-27 16:42:56 +0500

    created db connect in db.py modul

--- db.py
+++ db.py
@@ -1,6 +1,14 @@
-class Database:
-    pass
+import psycopg2 as psql
 
+class Database:
+    @staticmethod
+    def connect(query, query_type):
+        db = psql.connect(
+            database="test",
+            host="localhost",
+            user="postgres",
+            password="example_pass"
+        )
 
 class Model:
     pass
